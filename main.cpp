40b1f868989d23a038ed4e0c4e933bb72c6d2444
csdproductions <info@csdproductions.co.uk>
2020-05-08 15:21:55 +0000

    Part 1e completed (I think!)

--- main.cpp
+++ main.cpp
@@ -128,6 +128,28 @@ struct CarWash //                                   1) define an empty struct fo
     3) connect to network
  */
 
+struct GamesConsole
+{
+
+    //control pads
+    int noOfControlPads = 2;
+    // storage drive size
+    float storageDriveSize = 128.f;
+    // usb ports
+    int noOfUsbPorts = 4;
+    // hdmi outputs
+    int noOfHdmiOutputs = 3;
+    // network connections
+    int networkConnection = 2;
+
+    // read discs
+    void readDisc(std::string gameDisc = "Grand Theft Auto");
+    // play games
+    void playGame(int noOfPlayers = 8);
+    // connect to network - return if connection successful
+    bool connectToNetwork(float connectionSpeed = 1000.f);
+};
+
 /*
 2) Dog
 5 properties:
@@ -142,33 +164,97 @@ struct CarWash //                                   1) define an empty struct fo
     3) heel
  */
 
+struct Dog
+{
+    //fur colour
+    std::string furColour = "Brown";
+    //age
+    float age = 1.f;
+    //breed
+    std::string breed = "Shih-Tzu";
+    //name
+    std::string name = "Luna";
+    //sex
+    std::string sex = "Female";
+
+    //fetch 
+    void fetch(std::string ballType = "Tennis");
+    //sit
+    void sit(std::string treatType = "Peanut Butter");
+    //heel
+    void heel();
+};
+
 /*
 3) Bicycle
 5 properties:
-    1) frame
-    2) wheels
-    3) brakes
-    4) pedals
-    5) handlebars
+    1) frame size
+    2) wheel size
+    3) brakes model
+    4) pedals type
+    5) handlebars type
 3 things it can do:
     1) move forwards
     2) bunny hop
     3) wheelie
  */
 
+struct Bicycle
+{
+    //frame size
+    float frameSize = 51.1f;
+    //wheel size
+    float wheelRimSize = 18.f;
+    //brakes model
+    std::string brakesModel = "Shimano";
+    //pedals type (1 for standard, 2 for cleats)
+    int pedalsType = 1;
+    //handlebars type (1 for drops, 2 for straight)
+    int handlebarsType = 1;
+
+    //move forwards - return 
+    float moveForwards();
+    //bunny hop - return height
+    float bunnyHop();
+    //wheelie
+    bool popWheelie();
+};
+
 /*
 4) Electric Guitar
 5 properties:
-    1) body
-    2) neck
-    3) strings
-    4) pickups
-    5) frets
+    1) body wood
+    2) is neck joint glued
+    3) high e string gauge
+    4) pickups type
+    5) no of frets 
 3 things it can do:
     1) output sound
     2) change tone
     3) change pickup
  */
+
+struct ElectricGuitar{
+
+    //body wood - 1 = maple, 2 = oak
+    int bodyWood = 1;
+    //neck joint glued
+    bool neckJointGlued = false;
+    //strings gauge
+    int eStringGauge = 13;
+    //pickups type
+    std::string pickupsType = "Humbucker";
+    //no of frets 
+    int noOfFrets = 24;
+
+    //output sound
+    void outputSound(int fretPlayed);
+    //change tone - return tone resistance value
+    float changeTone(bool isSoloNow);
+    //change pickup - return
+    int selectPickup(int songStyle);
+};
+
 
 /*
 5) Staff
@@ -184,11 +270,32 @@ struct CarWash //                                   1) define an empty struct fo
     3) sign a contract
  */
 
+ struct Staff{
+
+    //type
+    std::string type = "Player";
+    //sex
+    std::string sex = "Male";
+    //age
+    int age = 22;
+    //hours of work
+    float hoursOfWork = 37.5f;
+    //days of work
+    int daysOfWork = 5;
+
+    //go to work
+    void goToWork();
+    //attend meeting
+    void attendMeeting();
+    //sign a contract
+    bool signContract(float contractOffer = 20000.f);
+ };
+
 /*
 6) program (male, female, or junior team)
 5 properties:
     1) sex
-    2) age range
+    2) maximum age
     3) professional status
     4) number of staff
     5) total number of programs
@@ -198,47 +305,177 @@ struct CarWash //                                   1) define an empty struct fo
     3) advertise for players
  */
 
+struct TeamProgram
+{
+    //sex
+    std::string sex = "Male";
+    //maximum age
+    int maxAge = 21;
+    //professional status
+    std::string profStatus = "Semi-Professional";
+    //number of staff
+    int noOfStaff = 6;
+    //total number of programs
+    int totalNoOfPlayers = 22;
+
+
+    //enter a league
+    void enterALeague();
+    //determine player's program
+    void playersProgram();
+    //advertise for players
+    void advertiseForPlayers(int howManySpacesLeft = 4);
+};
+
 /*
 7) Training Complex
 5 properties:
-    1) reception
-    2) canteen
-    3) gym
-    4) locker room
-    5) car Park
+    1) receptionist of the day
+    2) no of meals served in canteen a day
+    3) maximum deadlift weight in gym
+    4) no of lockers in locker room
+    5) no of car park spots
 3 things it can do:
     1) host training session
     2) feed staff
     3) provide player rehab
  */
 
+struct TrainingComplex
+{
+
+    //receptionist of the day
+    std::string receptionistName = "Dave";
+    //no of meals served in canteen a day
+    int noOfMealsServed = 122;
+    //maximum deadlift weight in gym
+    float maxDeadliftWeight = 500.f;
+    //no of lockers in locker room
+    int noOfLockers = 100;
+    //no of car park spots
+    int noOfCarParkSpots = 75;
+
+    //host training session
+    void hostTrainingSession();
+    //feed staff
+    void feedStaff(int howManyStaffWorkingToday = 75);
+    //provide player rehab
+    void providePlayerRehab(Staff player);
+};
+
+
 /*
 8) Stadium
 5 properties:
-    1) seats
-    2) bars
-    3) gates
-    4) executive boxes
-    5) staircases
+    1) capacity
+    2) name
+    3) turf type
+    4) bool executive boxes open
+    5) staircase names
 3 things it can do:
     1) host an event
     2) open gates
-    3) sell food
+    3) restock a concession stand
  */
 
+struct Stadium
+{
+    //capacity
+    int noOfSeats = 44000;
+    //stadium name
+    std::string name = "Paul Brown Stadium";
+    //turf type (1 = grass, 2 = turf)
+    int turfType = 1;
+    //are executive boxes open
+    bool boxesOpen = true;
+    //no of staircases
+    int noOfStaircases = 22;
+
+    struct ConcessionStand
+    {
+        //stand type (1 for food/drink, 2 for merchandise)
+        int standType = 1;
+        //does it have a liqour license
+        bool liquorLicense = false;
+        //min no of staff required to run it
+        int minNoOfStaff = 6;
+        //max no of staff required
+        int maxNoOfStaff = 8;
+        //name of stand
+        std::string name = "Dave's magical hot-dogs";
+
+        //check customer ID
+        bool isCustomerOfAge(int customerAge = 22);
+        //display which customer order will be ready next
+        int displayNextReadyCustomerOrders();
+        //take order from customer
+        void takeCustomerOrder();
+    };
+    
+    //host an event
+    void hostAnEvent();
+    //open gates
+    void openGates();
+    //restock a concession stand
+    void restockConcessionStand(ConcessionStand myConcessionStand);
+
+    ConcessionStand myConcessionStand;
+};
+
+
 /*
-9) Supporters
+9) Supporter Group
 5 properties:
-    1) total number
-    2) location
-    3) distance from club's city
-    4) member of supporters group
-    5) cheer team
+    1) name
+    2) no of members
+    3) no of season tickets
+    4) merch discount from club
+    5) cost of membership
 3 things it can do:
-    1) join supporters group
-    2) buy jersey
-    3) go to away match
+    1) arrange trip to away match
+    2) add a new member
+    3) email a member on their birthday
  */
+
+struct SupportersGroup
+{
+    std::string name = "Queen City Hooligans";
+    int noOfMembers = 120;
+    int noOfSeasonTickets = 60;
+    float merchDiscountFromClub = 10.f;
+    float costOfMembership = 100.f;
+
+    struct Supporter
+    {
+        //jersey size
+        std::string jerseySize = "M";
+        //location
+        std::string whereDoYouLive = "Cincinnati";
+        //distance from club's city
+        float distanceFromClubsCity = 123.f;
+        //season ticket holder
+        bool seasonTicketHolder = false;
+        //no of meetings attended
+        int noOfMeetingsAttended = 3;
+
+        // join supporters group
+        void joinSupportersGroup();
+        // buy jersey
+        void buyJersey();
+        // go to away match
+        void goToAwayMatch(std::string awayCity = "Salt Lake");
+    };
+
+    //Arrange trip to away match
+    void organiseAwayMatchTrip(int noOfSupporters = 20);
+    //Add a new member
+    void admitNewMember(Supporter mySupporter);
+    //Email a member on their birthday
+    void emailMemberOnBirthday(Supporter mySupporter);
+
+    Supporter mySupporter;
+};
+
 
 /*
 10) Sports Team
@@ -247,12 +484,33 @@ struct CarWash //                                   1) define an empty struct fo
     2) program
     3) training complex
     4) stadium
-    5) supporters
+    5) supporter
 3 things it can do:
     1) play a match
     2) run a training session
     3) sign a player
  */
+
+ struct SportsTeam
+ {
+    // staff
+    Staff staffMember;
+    // program
+    TeamProgram program;
+    //training complex
+    TrainingComplex complex;
+    //stadium
+    Stadium stadium;
+    //supporter
+    SupportersGroup mySupportersGroup;
+
+    // play a match
+    void playMatch(TeamProgram, Stadium);
+    // run a training session
+    void runTrainingSession(TeamProgram, TrainingComplex);
+    // sign a player
+    bool signAPlayer(Staff);
+ };
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
